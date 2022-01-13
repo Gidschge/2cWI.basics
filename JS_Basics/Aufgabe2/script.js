675849db791777f087b6d999542b69997d9168dc
Gideon <gideon.ruescher@student.htldornbirn.at>
2022-01-13 13:19:42 +0100

    finished JS 03

---
+++ JS_Basics/Aufgabe2/script.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+result = a / b
+
+console.log("The result of " + a + " divided by " + b + " is " + result)
+
+let firstname = "Gideon ";
+let lastname = "Rüscher";
+
+console.log("Ich heiße " + firstname + lastname);
---
+++ JS_Basics/Aufgabe3/script.js
@@ -0,0 +1,24 @@
+let banana = "Banane";
+let apple = "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleKilo = 0.34;
+let bananaKilo = 0.22;
+
+
+
+console.log("Anzahl Bananen pro Kilo ist " + 1 / bananaKilo)
+console.log("Anzahl Äpfel pro Kilo ist " + 1 / appleKilo)
+
+console.log("Preis pro Banane ist " + bananaPricePerKilo / 4.5454)
+console.log("Preis pro Apfel ist " + applePricePerKilo / 2.94)
+
+let PriceProBanane = bananaPricePerKilo / 4.5454;
+let PriceProApple = applePricePerKilo / 2.94;
+
+console.log("Preis von 8 Äpfeln ist " + PriceProApple * 8)
+console.log("Preis von 17 Bananen ist " + PriceProBanane * 17)
+console.log("Preis von 1 Tonne Bananen ist " + bananaPricePerKilo * 1000)
+console.log("Preis von 1 Tonne Äpfel ist " + applePricePerKilo * 1000)
