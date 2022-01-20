b574623d2319eb2bc9dfce5f591ff911369d691e
Gideon <gideon.ruescher@student.htldornbirn.at>
2022-01-20 14:27:41 +0100

    test finished

---
+++ JS_Basics/Test2cWI/java/aufgabe1/script.js
@@ -0,0 +1,7 @@
+for (let count = 1; count <= 8; count++) {
+
+    console.log(count + " ")
+
+}
+
+console.log("Das wars!")
---
+++ JS_Basics/Test2cWI/java/aufgabe2/script.js
@@ -0,0 +1,3 @@
+for (let number = 11; number > -4; number = number - 2) {
+    console.log(number)
+}
