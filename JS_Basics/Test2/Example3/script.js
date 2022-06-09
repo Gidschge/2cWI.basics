33548fba41f3c8f3172e63aa2830082e1d509624
Gideon <gideon.ruescher@student.htldornbirn.at>
2022-06-09 14:21:27 +0200

    Ersten 2 Aufgaben fertig

---
+++ JS_Basics/Test2/Example3/script.js
@@ -0,0 +1,10 @@
+let random = Math.random() * 100
+
+let randomAsInteger = Math.round(random);
+
+console.log(randomAsInteger);
+
+
+for (let count = randomAsInteger; count = 10; count--) {
+    console.log(count)
+}
